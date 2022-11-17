1bb7b065d4881ab8b67eaca8536f65dc341237c0
Natalia Rusu <bonapartik@gmail.com>
2022-11-17 14:11:14 +0000

    "add methods to Department class" "call methods on devDepartment, the instance of Department class"

--- dist/app.js
+++ dist/app.js
@@ -1,17 +1,23 @@
 "use strict";
 class Department {
     constructor(n) {
+        this.employees = [];
         this.name = n;
     }
     describe() {
+        //should add that 'this' will be the instance of Department
         console.log(`Department: ${this.name}`);
+    }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmploeeInfo() {
+        console.log(this.employees);
     }
 }
 const devDepartment = new Department('Development'); //{name: 'Development'}
 devDepartment.describe(); //Department: Development
-const testD1 = { describe: devDepartment.describe }; //we pointing to the method
-//testD1.describe(); //Department: undefined , because we call describe on testD, not on the obj that is indstance of Department (ts error)
-const testD2 = { name: 'Testing', describe: devDepartment.describe }; //ts doesn't catch the error, because testD2 has 'name' property?
-testD2.describe();
-console.log(testD2 instanceof Department); //false
+devDepartment.addEmployee('Max');
+devDepartment.addEmployee('Kate');
+devDepartment.printEmploeeInfo();
 //# sourceMappingURL=app.js.map
