71817b726bff9865d4bafbf73b6758a6479aba60
Natalia Rusu <bonapartik@gmail.com>
2022-11-17 14:00:42 +0000

    test 'this' keyword in classes

--- dist/app.js
+++ dist/app.js
@@ -3,5 +3,15 @@ class Department {
     constructor(n) {
         this.name = n;
     }
+    describe() {
+        console.log(`Department: ${this.name}`);
+    }
 }
+const devDepartment = new Department('Development'); //{name: 'Development'}
+devDepartment.describe(); //Department: Development
+const testD1 = { describe: devDepartment.describe }; //we pointing to the method
+//testD1.describe(); //Department: undefined , because we call describe on testD, not on the obj that is indstance of Department (ts error)
+const testD2 = { name: 'Testing', describe: devDepartment.describe }; //ts doesn't catch the error, because testD2 has 'name' property?
+testD2.describe();
+console.log(testD2 instanceof Department); //false
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,20 @@
-class Department{
-    name: string;
+class Department {
+  name: string;
 
-    constructor(n:string){
-        this.name=n;
-    }
+  constructor(n: string) {
+    this.name = n;
+  }
+  describe(this: Department) {//should add that 'this' will be the instance of Department
+    console.log(`Department: ${this.name}`);
+  }
 }
+
+const devDepartment = new Department('Development'); //{name: 'Development'}
+devDepartment.describe(); //Department: Development
+
+const testD1 = { describe: devDepartment.describe }; //we pointing to the method
+//testD1.describe(); //Department: undefined , because we call describe on testD, not on the obj that is indstance of Department (ts error)
+
+const testD2 = { name: 'Testing', describe: devDepartment.describe }; //ts doesn't catch the error, because testD2 has 'name' property?
+testD2.describe();
+console.log(testD2 instanceof Department)//false
