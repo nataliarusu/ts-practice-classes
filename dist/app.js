6b3d36ed5c554ace4a2801aca0e9a4661aa8d3d9
Natalia Rusu <bonapartik@gmail.com>
2022-11-23 09:01:21 +0000

    shorthand initialization

--- dist/app.js
+++ dist/app.js
@@ -1,12 +1,19 @@
 "use strict";
 class Department {
-    constructor(n) {
+    constructor(name, id) {
+        this.name = name;
+        this.id = id;
+        // private name: string;
+        // private id: string;
         this.employees = []; //force employee to be added only using addEmployee() method
-        this.name = n;
+        //private and public infront of args it is shorthand initialization
+        //instead of creating these 2 lines we can initialize properties in one line
+        // this.name = name;
+        //this.id = id;
     }
     getDepartment() {
         //'this' will be the instance of Department
-        return `Department: ${this.name}`;
+        return `Department: ${this.name}, ${this.id}`;
     }
     addEmployee(employee) {
         this.employees.push(employee);
@@ -15,7 +22,7 @@ class Department {
         console.log(this.employees);
     }
 }
-const devDepartment = new Department('Development'); //{name: 'Development'}
+const devDepartment = new Department('Development', '1'); //{name: 'Development'}
 console.log(devDepartment.getDepartment()); //Department: Development
 devDepartment.addEmployee('Max');
 devDepartment.addEmployee('Kate');
--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,27 @@
 class Department {
-  private name: string;
-  private employees: string[] = [];//force employee to be added only using addEmployee() method
+  // private name: string;
+  // private id: string;
+  private employees: string[] = []; //force employee to be added only using addEmployee() method
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(private name: string, private id: string) {
+    //private and public infront of args it is shorthand initialization
+    //instead of creating these 2 lines we can initialize properties in one line
+    // this.name = name;
+    //this.id = id;
   }
   getDepartment(this: Department) {
     //'this' will be the instance of Department
-    return `Department: ${this.name}`;
+    return `Department: ${this.name}, ${this.id}`;
   }
-  addEmployee(employee:string){
+  addEmployee(employee: string) {
     this.employees.push(employee);
   }
-  printEmploeeInfo(){
+  printEmploeeInfo() {
     console.log(this.employees);
   }
 }
 
-const devDepartment = new Department('Development'); //{name: 'Development'}
+const devDepartment = new Department('Development', '1'); //{name: 'Development'}
 console.log(devDepartment.getDepartment()); //Department: Development
 devDepartment.addEmployee('Max');
 devDepartment.addEmployee('Kate');
